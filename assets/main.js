cfda68ef62a02d4586a99b06f2762d8da3b6ba43
Imran_Diva <imrandiva01@gmail.com>
2023-08-27 09:50:21 +0200

    show-relevant-times

--- assets/main.js
+++ assets/main.js
@@ -35,11 +35,42 @@ document.addEventListener("DOMContentLoaded", () => {
             pray_id = ["fajr", "sunrise", "dhuhr", "asr", "maghrib", "isha"]
 
             var i = 0;
+            active = false
             for (prayer of prayerNameList) {
+            
+            prayer_time = dict[prayer]
+                                            // Replace this with the time string you have
+            const timeString = prayer_time // Example time string in HH:MM:SS format
+
+            const today = new Date();
+
+            
+            const year = today.getFullYear();
+            const month = (today.getMonth() + 1).toString().padStart(2, '0');
+            const day = today.getDate().toString().padStart(2, '0');
+
+
+            // Step 1: Parse the time string into a Date object
+
+            const parsedTime = new Date(`${year}-${month}-${day}T${timeString}`);
+
+            // Step 3: Compare the two Date objects
+            if (parsedTime < today) {
+                continue
+            }
+
                 var prayer_id = document.getElementById(pray_id[i]);
                 
                 
                 var mydiv = document.createElement("div");
+                
+                if (active == false) {
+                    prayer_id.style.paddingTop =  "1rem"
+                    prayer_id.style.paddingBottom  =  "1rem"
+                    prayer_id.style.borderStyle  =  "Solid"
+                    prayer_id.style.borderColor  =  "#03DAC6"
+                    active = true
+                }
 
                 var img = document.createElement("img");
                 img.src = "./assets/img/sunrise.svg"
@@ -69,7 +100,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 const temp2 = document.createElement("p");
                 mydiv = document.createElement("div");
                 // mydiv.style.display = "flex"
-                temp2.textContent = dict[prayer]
+                temp2.textContent = prayer_time
                 temp2.style.fontSize = "2rem"
                 mydiv.appendChild(temp2)
 
